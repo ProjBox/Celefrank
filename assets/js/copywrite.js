82f8206b4ac420333639e3e13b002131340ed472
Ben-Alley <121666458+Ben-Alley@users.noreply.github.com>
2026-07-03 13:24:48 +0100

    Updated Copywrite time Script
    Updated Copywrite time Script.
    ID added to copywrite footer tag

---
+++ assets/js/copywrite.js
@@ -0,0 +1,8 @@
+
+// =>> Copy Write
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.getElementById("copyright-year");
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
